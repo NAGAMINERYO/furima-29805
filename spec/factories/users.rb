b91e55813b0a6e27d39d2903ccdce9db8826b358
NAGAMINERYO <70889037+NAGAMINERYO@users.noreply.github.com>
2020-10-13 17:05:24 +0900

    単体テストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    nickname {Faker::Name.name}
+    last_name  { "山田" }
+    first_name { "太郎" }
+    last_name_kana { "ヤマダ" }
+    first_name_kana { "タロウ" }
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    # password = Faker::Internet.password(min_length: 6)
+    password {"Pa2020"}
     password_confirmation {password}
+    birthday { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    
   end
 end
